ebd53ef0d0c8f53f51e7ae2a90763c0e685ddcc0
dimka <dmitryellison@gmail.com>
2023-05-03 13:53:32 +0300

    print our voice

--- src/lab4.py
+++ src/lab4.py
@@ -1,5 +1,7 @@
 # import modele that can convert voice to text
-import pyttsx3
+import json
+
+import pyttsx3, pyaudio, vosk
 
 tts = pyttsx3.init()
 
@@ -11,5 +13,37 @@ for voice in voices:
     if voice == "Microsoft Irina Desktop - Russian":
         tts.setProperty('voice', voice.id)
 
-tts.say("Привет")
-tts.runAndWait()
+
+def say_something(text: str):
+    tts.say(text)
+    tts.runAndWait()
+
+
+model = vosk.Model('model_small')
+record = vosk.KaldiRecognizer(model, 16000)
+pa = pyaudio.PyAudio()
+stream = pa.open(format=pyaudio.paInt16,
+                 channels=1,
+                 rate=16000,
+                 input=True,
+                 frames_per_buffer=8000)
+
+stream.start_stream()
+
+
+def speak(say):
+    tts.say(say)
+    tts.runAndWait()
+
+
+def listen():
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+        if record.AcceptWaveform(data) and len(data) > 0:
+            answer = json.loads(record.Result())
+            if answer['text']:
+                yield answer['text']
+
+
+for text in listen():
+    print(text)
