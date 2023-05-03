6e1ecb1a9a2a29c37781284855caa3c9944b0c30
dimka <dmitryellison@gmail.com>
2023-05-03 13:42:07 +0300

    say Hello

--- src/lab4.py
+++ src/lab4.py
@@ -4,6 +4,12 @@ import pyttsx3
 tts = pyttsx3.init()
 
 voices = tts.getProperty('voices')
+tts.setProperty('voices', 'en')
 
 for voice in voices:
     print(voice.name)
+    if voice == "Microsoft Zira Desktop - English (United States)":
+        tts.setProperty('voice', voice.id)
+
+tts.say("Hello")
+tts.runAndWait()
